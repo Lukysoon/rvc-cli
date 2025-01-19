db27f52cd7aba1cc6872c1ec94ab01491227bdc3
Lukas Starenko <test.mopnx@passinbox.com>
2025-01-19 15:57:31 +0300

    install jp kernel

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@
 mkdir -p datasets
 mkdir -p pretrained
 
+python3 -m ipykernel install --user --name=venv
+
 python3 -m venv venv
+
 source venv/bin/activate
 
 pip install -r requirements.txt
 
-python3 -m ipykernel install --user --name=venv
 
 python3 rvc_cli.py prerequisites

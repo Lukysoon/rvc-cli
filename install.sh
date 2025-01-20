d8c5cc17eca407e2cf1346079dbe1d6940892ecc
Lukas Starenko <test.mopnx@passinbox.com>
2025-01-20 16:49:07 +0300

    update install sh

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
-#!/bin/bash
+pip install --upgrade pip
+
+# create base directories
 mkdir -p datasets
 mkdir -p pretrained
 
-python3 -m ipykernel install --user --name=venv
-
+# create and activate virtual env
 python3 -m venv venv
-
 source venv/bin/activate
 
-pip install -r requirements.txt
+# install necessary libraries
 
+# install jupyter kernel to virtual environment 
+pip install ipykernel
+python3 -m ipykernel install --user --name=venv
+
+apt update
+apt install ffmpeg
+
+pip install -r requirements.txt
 
 python3 rvc_cli.py prerequisites

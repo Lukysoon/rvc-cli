2de90aed79f7f61d5d81978303ebdef46284826d
Lukas Starenko <starenko.lukas@pm.me>
2025-04-02 21:38:38 +0200

    extra large

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ source venv/bin/activate
 
 pip install -r requirements.txt
 
-mkdir hubert_large
-cd hubert_large
-wget https://huggingface.co/facebook/hubert-large-ls960-ft/resolve/main/pytorch_model.bin
-wget https://huggingface.co/facebook/hubert-large-ls960-ft/resolve/main/config.json
+mkdir hubert_extra_large
+cd hubert_extra_large
+wget https://huggingface.co/facebook/hubert-xlarge-ll60k/resolve/main/pytorch_model.bin
+wget https://huggingface.co/facebook/hubert-xlarge-ll60k/resolve/main/config.json
 cd ..
 
 python3 rvc_cli.py prerequisites
